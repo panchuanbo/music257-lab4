8780ba750cd4e91549c777e9ebf9726870fa3b6a
panchuanbo <panchuanbo@yahoo.com>
2018-05-07 11:17:14 -0700

    enemies now move!

---
+++ biofeedback/Assets/Scripts/Enemies/EnemyMover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour {
+
+	public float enemySpeed = 0.05f;
+
+	// Update is called once per frame
+	void Update() {
+		// Move!
+		float zPos = transform.position.z - enemySpeed;
+		Vector3 enemyPos = new Vector3(transform.position.x, transform.position.y, zPos);
+		transform.position = enemyPos;
+	}
+}
--- biofeedback/Assets/Scripts/GM.cs
+++ biofeedback/Assets/Scripts/GM.cs
@@ -35,7 +35,7 @@ public class GM : MonoBehaviour {
 	}
 
 	private void Update() {
-		// spawnEnemy();
+		
 	}
 
 	private void spawnEnemy() {
